c48e1a743d307effb2da6f569e9f3f2161189cb7
Matthew Gottsacker <matt@matthews-mbp.home>
2018-03-22 16:38:01 -0500

    added request dependency

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const
   express = require('express'),
   bodyParser = require('body-parser'),
+  request = require('request'),
   app = express().use(bodyParser.json()); // creates express http server
 
 
